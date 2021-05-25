2a9c170be36277e3fdfea58ade867272176411d5
rachit2501 <rac.sri25@gmail.com>
2021-05-25 19:13:58 +0530

    wrapper basic features

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+#### 1.0.0 (2021-05-25)
+
+##### Other Changes
+
+- liquidity provider functions, and getLoan function. Testing left ([fd5cf169](https://github.com/Insureably/Protocol/commit/fd5cf169f3c8539ef8558561dbbfe3fe4ec8d7e5))
--- contracts/ERC20Wrapper.sol
+++ contracts/ERC20Wrapper.sol
@@ -1,16 +1,21 @@
 pragma solidity >0.8.0;
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract Wrapper is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable, ERC20Pausable {
-
+contract Wrapper is
+    ERC20,
+    ERC20Detailed,
+    ERC20Mintable,
+    ERC20Burnable,
+    ERC20Pausable
+{
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
@@ -18,18 +23,25 @@ contract Wrapper is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable, ERC20Pau
         uint256 time;
         uint256 amount;
     }
-    // to store the address of 
-    IERC20 public immutable coin;
-    address immutable factoryContract; 
+    // to store the address of
+    IERC20 public immutable Coin;
+    address immutable factoryContract;
     uint256 totalLiquidity;
     uint256 usedLiquidity;
-    mapping(address=>uint256) liquidityMapping;
+    address[] balanceSupplyCallPending;
+    mapping(address => uint256) liquidityMapping;
     mapping(address => BorrowerDetails) borrowersMapping;
 
-    contructor(address token, bytes[]32 name, bytes[]32 symbol, uint256 precision) ERC20Detailed(name,symbol,precision) public {
-      coin = _tokenAddress;
-      factoryContract = msg.sender;
-      addMinter(msg.sender);
+    constructor(
+        address _tokenAddress,
+        bytes32[] name,
+        bytes32[] symbol,
+        uint256 precision,
+        address admin
+    ) public ERC20Detailed(name, symbol, precision) {
+        Coin = _tokenAddress;
+        factoryContract = msg.sender;
+        addMinter(msg.sender);
     }
 
     /* the LenderLoan contract takes permission to spend a particular ERC20 on behalf of the liquidity provider. 
@@ -41,30 +53,104 @@ contract Wrapper is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable, ERC20Pau
         liquidity += amount;
     }
 
-    function decreaseSupply(uint256 amount , address sender) public onlyMinter {
+    function decreaseSupply(uint256 amount, address sender) public onlyMinter {
         uint256 availaibleSupply = getAvailaibleSupply();
-        
-        require(availaibleSupply >= amount,"not enough tokens to available. Please wait for some more time");
-        require(amount <= liquidityMapping[sender] , "not enough liquidity provided by the user");
+
+        require(
+            availaibleSupply >= amount,
+            "not enough tokens to available. Please wait for some more time"
+        );
+        require(
+            amount <= liquidityMapping[sender],
+            "not enough liquidity provided by the user"
+        );
 
         totalLiquidity.sub(amount);
-        coin.safeTransfer(sender,amount);
+        Coin.safeTransfer(sender, amount);
     }
 
-    function getLoan(address borrower, uint256 numberOfDays, uint256 amount) public onlyMinter {
-        require(amount <= getAvailaibleAmount() , "not enough liquidity");    
+    function getLoan(
+        address borrower,
+        uint256 numberOfDays,
+        uint256 amount
+    ) public onlyMinter {
+        require(amount <= getAvailaibleAmount(), "not enough liquidity");
         usedLiquidity.add(amount);
-        borrowersMapping[borrower] += BorrowerDetails( now + numberOfDays * 1 days,amount);
-        _mint(borrower,amount);
+        addBorrower(borrower, now + numberOfDays * 1 days, amount);
+        _mint(borrower, amount);
     }
 
-    // function paybackLoan(uint256 amount , ) public {
-    //     require(amount <= borrowsersMapping[borrower], "you weren't given this much liquidity. Please repay your own loan only");
-    //     borrowersMapping[msg.sender]
-    // }
-    
- 
+    function paybackLoan(uint256 amount) public {
+        require(
+            amount <= borrowsersMapping[borrower],
+            "you weren't given this much liquidity. Please repay your own loan only"
+        );
+
+        borrowersMapping[msg.sender].amount;
+    }
+
     function getAvailaibleSupply() public returns (uint256) {
         return totalLiquidity.sub(usedLiquidity);
+    }
+
+    function balanceSupply() public {
+        uint256 callerProfit = 0;
+        address iterator;
+
+        for (int256 x = 0; x < balanceSupplyCallPending.length; x++) {
+            iterator = balanceSupplyCallPending[x];
+            BorrowerDetails storage borrower = borrowersMapping[iterator];
+            if (borrower.time > now) {
+                callerProfit += borrower.amount;
+                burnFrom(iterator, borrower.amount);
+                balanceSupplyCallPending[x] = balanceSupplyCallPending[
+                    balanceSupplyCallPending.length - 1
+                ];
+                balanceSupplyCallPending.length--;
+                x--;
+            }
+        }
+
+        uint256 reward = callerProfit.div(100);
+        Coin.safeTransfer(msg.sender, reward);
+    }
+
+    function addBorrower(
+        uint256 recipient,
+        uint256 time,
+        uint256 amount
+    ) internal {
+        borrowersMapping[recipient] += BorrowerDetails(time, amount);
+        balanceSupplyCallPending.push(recipient);
+    }
+
+    // Overridden functions
+    function transfer(address recipient, uint256 amount)
+        public
+        virtual
+        override
+        returns (bool)
+    {
+        _transfer(_msgSender(), recipient, amount);
+        addBorrower(recipient, borrowersMapping[msg.sender].time, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        _transfer(sender, recipient, amount);
+
+        uint256 currentAllowance = _allowances[sender][_msgSender()];
+        require(
+            currentAllowance >= amount,
+            "ERC20: transfer amount exceeds allowance"
+        );
+        _approve(sender, _msgSender(), currentAllowance - amount);
+        addBorrower(recipient, borrowersMapping[msg.sender].time, amount);
+
+        return true;
     }
 }
