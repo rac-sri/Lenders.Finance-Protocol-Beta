1ff682b104c5a2a87b21cd4a3500ffd1f3239a74
rachit2501 <rac.sri25@gmail.com>
2021-07-04 21:58:18 +0530

    config

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,11 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider")
+require("dotenv").config()
 
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1", // Localhost (default: none)
-      port: 7545, // Custom port
+      port: 9545, // Custom port
       network_id: "*", // Custom network
       gas: 6721975, // Gas sent with each transaction (default: ~6700000)
       // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
@@ -29,9 +30,31 @@ module.exports = {
           `https://rinkeby.infura.io/v3/${process.env.infuraKey}`
         )
       },
-      network_id: 4, // Ropsten's id
+      network_id: 4, // rinkeby's id
     },
 
+    matic: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.mnemonic,
+          `https://rpc-mumbai.matic.today`
+        ),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
+
+    harmony: {
+      provider: () => {
+        return new HDWalletProvider({
+          mnemonic: process.env.mnemonic,
+          providerOrUrl: "https://api.s0.b.hmny.io", // https://api.s0.t.hmny.io for mainnet
+          derivationPath: `m/44'/1023'/0'/0/`,
+        })
+      },
+      network_id: 1666700000, // 1666600000 for mainnet
+    },
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
