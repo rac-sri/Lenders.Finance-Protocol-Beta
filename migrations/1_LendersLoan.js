c717b31143414c082453ee81cf69c02f128c2c97
rachit2501 <rac.sri25@gmail.com>
2021-07-05 12:35:07 +0530

    remix and truffle test cases passing.Problem only on ui

--- migrations/1_LendersLoan.js
+++ migrations/1_LendersLoan.js
@@ -5,31 +5,35 @@ const UNERC20 = artifacts.require("UNERC20")
 const ProxyContract = artifacts.require("ERC1967Proxy")
 const DataProvider = artifacts.require("DataProvider")
 const InterestProvider = artifacts.require("InterestRateStatergy")
-const { targetERC20, name, symbol, admin } = config
+const ERC20 = artifacts.require("Dai")
+
+// , dai.address, "Dai", "dai", admin
 
 module.exports = async function (deployer) {
-  deployer.deploy(UNERC20, targetERC20, name, symbol, admin).then((result) => {
-    return deployer
-      .deploy(ProxyContract, result.address, "0x")
-      .then((proxy) => {
-        return deployer.deploy(DataProvider).then((dp) => {
-          return deployer.deploy(InterestProvider).then(async (it) => {
-            const itInstance = await InterestProvider.deployed()
-            await itInstance.initialize(dp.address, 5)
-            return deployer
-              .deploy(
-                LendersFactory,
-                proxy.address,
-                result.address,
-                dp.address,
-                it.address
-              )
-              .then(async (res) => {
-                const DataProviderInstance = await DataProvider.deployed()
-                await DataProviderInstance.initialize(10, 5, res.address)
-              })
+  deployer.deploy(ERC20, 10000).then(async (dai) => {
+    return deployer.deploy(UNERC20).then((result) => {
+      return deployer
+        .deploy(ProxyContract, result.address, "0x")
+        .then((proxy) => {
+          return deployer.deploy(DataProvider).then((dp) => {
+            return deployer.deploy(InterestProvider).then(async (it) => {
+              const itInstance = await InterestProvider.deployed()
+              await itInstance.initialize(dp.address, 5)
+              return deployer
+                .deploy(
+                  LendersFactory,
+                  proxy.address,
+                  result.address,
+                  dp.address,
+                  it.address
+                )
+                .then(async (res) => {
+                  const DataProviderInstance = await DataProvider.deployed()
+                  await DataProviderInstance.initialize(10, 5, res.address)
+                })
+            })
           })
         })
-      })
+    })
   })
 }
